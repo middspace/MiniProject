e556dc73728a6a68a66893088d591e164682c186
Jacob Schnitker <jacobschnitker@gmail.com>
2023-02-09 12:54:35 -0500

    Had to recode the function for gettting the image, kept getting a strange error. Also added a few comments

--- app/src/main/java/com/example/miniproject/MainActivity.java
+++ app/src/main/java/com/example/miniproject/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.miniproject;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -13,14 +15,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView pic;
     Button select;
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,31 +34,43 @@ public class MainActivity extends AppCompatActivity {
 
         pic = findViewById(R.id.IVPic);
         select = findViewById(R.id.btnLoadImg);
+        text = findViewById(R.id.tvGuess);
 
         select.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadImg();
+
+                //sets the click event for load image button
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+
+                startActivityForResult(Intent.createChooser(intent,"Pick an image"), 1);
+
             }
+
         });
     }
 
+    //this method handles grabbing the image and converting it into a bitmap
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 1) {
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(data.getData());
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                pic.setImageBitmap(bitmap);
+                text.setText(pickString());
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
     public void loadImg(){
-        ActivityResultLauncher<String> selectPhoto = registerForActivityResult(new ActivityResultContracts.GetContent(),
-                uri -> {
-                    if (uri != null) {
-                        try {
-                            Bitmap selectedImg = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                            pic.setImageBitmap(selectedImg);
-                        }
-                        catch(IOException e){
-                            Log.e("IMAHE SELECTION ERROR: ", e.toString());
-                        }
-                    }
-                }
-        );
-        pickString();
+
     }
 
     private String pickString(){
