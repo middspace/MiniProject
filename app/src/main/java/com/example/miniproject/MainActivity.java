90e79c4b7057da1bf5d6f8520a9736cbad8cc887
Jacob Schnitker <jacobschnitker@gmail.com>
2023-02-09 11:22:06 -0500

    Setting up view

--- app/src/main/java/com/example/miniproject/MainActivity.java
+++ app/src/main/java/com/example/miniproject/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.miniproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView pic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        pic = findViewById(R.id.IVPic);
+    }
+
+
+    public void loadImg(View view){
+
     }
 }
